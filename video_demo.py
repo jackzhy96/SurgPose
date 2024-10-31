8ba09a10d5c75714f85d21a7e92356b2d08b35b8
Zijian Wu <zijianwu@ece-cdd-ts13.ece.ubc.ca>
2024-10-30 19:30:34 -0700

    sam script

--- video_demo.py
+++ video_demo.py
@@ -9,6 +9,26 @@ from PIL import Image
 import cv2
 import yaml
 
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--video_dir", type=str, default="./data/left_frames")
+parser.add_argument("--kps_dir", type=str, default="./kps_annotation")
+parser.add_argument("--save_vis", action="store_true")
+parser.add_argument("--res_vis_dir", type=str, default="./results")
+
+args = parser.parse_args()
+
+ann_config = {
+    "psm": "psm1",
+    "traj_idx": 000000,
+    "left_or_right": "left",
+}
+traj_idx = ann_config["traj_idx"]
+left_or_right = ann_config["left_or_right"]
+psm = ann_config["psm"]
+
 # use bfloat16 for the entire notebook
 torch.autocast(device_type="cuda", dtype=torch.bfloat16).__enter__()
 
@@ -19,8 +39,8 @@ if torch.cuda.get_device_properties(0).major >= 8:
 
 from sam2.build_sam import build_sam2_video_predictor
 
-sam2_checkpoint = "./checkpoints/sam2_hiera_large.pt"
-model_cfg = "sam2_hiera_l.yaml"
+sam2_checkpoint = "./checkpoints/sam2.1_hiera_large.pt" # sam2_checkpoint = "../checkpoints/sam2.1_hiera_large.pt"
+model_cfg = "configs/sam2.1/sam2.1_hiera_l.yaml"        # model_cfg = "sam2_hiera_l.yaml"
 
 predictor = build_sam2_video_predictor(model_cfg, sam2_checkpoint)
 
@@ -42,14 +62,9 @@ def show_points(coords, labels, ax, marker_size=200):
     ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
     ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
 
-# `video_dir` a directory of JPEG frames with filenames like `<frame_index>.jpg`
-# video_dir = '/bigdata/SurgPose/000028/regular/left_frames' # For SurgPose
-video_dir = '/home/zijianwu/Downloads/STIR_SAM_Demo/22seq13' # For Ethan
-
-
 # scan all the JPEG frame names in this directory
 frame_names = [
-    p for p in os.listdir(video_dir)
+    p for p in os.listdir(args.video_dir)
     # if os.path.splitext(p)[-1] in [".jpg", ".jpeg", ".JPG", ".JPEG", 'png']
 ]
 frame_names.sort(key=lambda p: int(os.path.splitext(p)[0][5:]))
@@ -57,11 +72,11 @@ frame_names.sort(key=lambda p: int(os.path.splitext(p)[0][5:]))
 frame_idx = 0
 plt.figure(figsize=(12, 8))
 plt.title(f"frame {frame_idx}")
-plt.imshow(Image.open(os.path.join(video_dir, frame_names[frame_idx])))
+plt.imshow(Image.open(os.path.join(args.video_dir, frame_names[frame_idx])))
 plt.show()
 breakpoint()
 
-inference_state = predictor.init_state(video_path=video_dir)
+inference_state = predictor.init_state(video_path=args.video_dir)
 
 predictor.reset_state(inference_state)
 
@@ -70,17 +85,17 @@ prompts = {}  # hold all the clicks we add for visualization
 ## PSM1
 ann_frame_idx = 0  # the frame index we interact with
 ann_obj_id = 1  # psm1 shaft
-points = np.array([[103, 124], [339, 244], [209, 312]], dtype=np.float32) #np.array([[1223, 542], [1204, 537], [1221, 513], [1253, 542], [1221, 555]], dtype=np.float32)
-labels = np.array([1, 1, 1], np.int32)
+points = np.array([[139, 413], [116, 425]], dtype=np.float32) #np.array([[1223, 542], [1204, 537], [1221, 513], [1253, 542], [1221, 555]], dtype=np.float32)
+labels = np.array([1, 0], np.int32)
 prompts[ann_obj_id] = points, labels
 _, out_obj_ids, out_mask_logits = predictor.add_new_points(inference_state=inference_state, frame_idx=ann_frame_idx, obj_id=ann_obj_id, points=points, labels=labels,)
 
-ann_frame_idx = 0  # the frame index we interact with
-ann_obj_id = 2  # psm1 wrist
-points = np.array([[1230, 635], [1128, 701], [1032, 763]], dtype=np.float32)
-labels = np.array([1, 1, 1], np.int32)
-prompts[ann_obj_id] = points, labels
-_, out_obj_ids, out_mask_logits = predictor.add_new_points(inference_state=inference_state, frame_idx=ann_frame_idx, obj_id=ann_obj_id, points=points, labels=labels,)
+# ann_frame_idx = 0  # the frame index we interact with
+# ann_obj_id = 2  # psm1 wrist
+# points = np.array([[1230, 635], [1128, 701], [1032, 763]], dtype=np.float32)
+# labels = np.array([1, 1, 1], np.int32)
+# prompts[ann_obj_id] = points, labels
+# _, out_obj_ids, out_mask_logits = predictor.add_new_points(inference_state=inference_state, frame_idx=ann_frame_idx, obj_id=ann_obj_id, points=points, labels=labels,)
 
 # ann_frame_idx = 0  # the frame index we interact with
 # ann_obj_id = 3  # psm1 joint 1
@@ -171,7 +186,7 @@ _, out_obj_ids, out_mask_logits = predictor.add_new_points(inference_state=infer
 ann_frame_idx = 0
 plt.figure(figsize=(12, 8))
 plt.title(f"frame {ann_frame_idx}")
-plt.imshow(Image.open(os.path.join(video_dir, frame_names[ann_frame_idx])))
+plt.imshow(Image.open(os.path.join(args.video_dir, frame_names[ann_frame_idx])))
 # show_points(points, labels, plt.gca())
 for i, out_obj_id in enumerate(out_obj_ids):
     # show_points(*prompts[out_obj_id], plt.gca())
@@ -210,23 +225,23 @@ for out_frame_idx, out_obj_ids, out_mask_logits in predictor.propagate_in_video(
         for i, out_obj_id in enumerate(out_obj_ids)
     }
 
-# with open("./keypoints/keypoints_000029_right_psm3.yaml", "w") as f: 
-#     yaml.dump(video_keypoints, f)
+os.makedirs(args.kps_dir, exist_ok=True)
 
-breakpoint()
+with open(os.path.join(args.kps_dir, f"keypoints_{traj_idx}_{left_or_right}_{psm}.yaml"), "w") as f: 
+    yaml.dump(video_keypoints, f)
 
-# render the segmentation results every few frames
-# video_dir = '/bigdata/SurgPose/000000/regular/left_frames'
-vis_frame_stride = 1
-os.makedirs("./results", exist_ok=True)
-plt.close("all")
-for out_frame_idx in range(0, len(frame_names), vis_frame_stride):
-    plt.figure(figsize=(6, 4))
-    # plt.title(f"frame {out_frame_idx}")
-    plt.imshow(Image.open(os.path.join(video_dir, frame_names[out_frame_idx])))
-    for out_obj_id, out_mask in video_segments[out_frame_idx].items():
-        show_mask(out_mask, plt.gca(), obj_id=out_obj_id)
-    # save the visualization to disk
-    plt.axis('off')
-    plt.savefig(f"./results/frame_{out_frame_idx:04d}.png", bbox_inches='tight', pad_inches=0)
-    plt.close()
+if args.save_vis:
+    # render the segmentation results every few frames
+    vis_frame_stride = 1
+    os.makedirs(args.res_vis_dir, exist_ok=True)
+    plt.close("all")
+    for out_frame_idx in range(0, len(frame_names), vis_frame_stride):
+        plt.figure(figsize=(6, 4))
+        plt.imshow(Image.open(os.path.join(args.video_dir, frame_names[out_frame_idx])))
+        for out_obj_id, out_mask in video_segments[out_frame_idx].items():
+            show_mask(out_mask, plt.gca(), obj_id=out_obj_id)
+        # save the visualization to disk
+        plt.axis('off')
+        # plt.savefig(f"./results/frame_{out_frame_idx:04d}.png", bbox_inches='tight', pad_inches=0)
+        plt.savefig(os.path.join(args.res_vis_dir, f"frame_{out_frame_idx:04d}.png"), bbox_inches='tight', pad_inches=0)
+        plt.close()
